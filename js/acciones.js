bc8892aafd1fe04ed549f5daa77a505ae92741f8
FherCopal <38224705+FherCopal@users.noreply.github.com>
2019-05-02 08:42:02 -0500

    5

--- js/acciones.js
+++ js/acciones.js
@@ -4,11 +4,11 @@ $(document).ready(function(e){
 document.addEventListener("deviceready",function( ){
 
 $('#beep').tap(function(){
-navigator. notification .beep(1);
+navigator.notification.beep(1);
 });//tap beep
 
 $('#vibrar').tap(function(){
-navigator.notification.vibrate(1000);
+navigator.notification.vibrate(3000);
 });//tap vibrar
 
 },false); //deviceready
